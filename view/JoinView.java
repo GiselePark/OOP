97966186c2eddc4328255848009ae1766f11ad4e
GiselePark <hanied04@gmail.com>
2024-02-26 18:32:45 +0900

    Feat - JoinView

--- view/JoinView.java
+++ view/JoinView.java
@@ -6,8 +6,7 @@ import model.UserDto;
 import java.util.Scanner;
 
 public class JoinView {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(Scanner sc) {
         System.out.println("ID,비밀번호, 비밀번호 확인, 이름, 주민번호, 전화번호, 주소, 직업을 입력해 주세요.");
         UserDto user = new UserBuilder()
                 .username(sc.next())
