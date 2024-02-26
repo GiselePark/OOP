45b9431bd504fbe3181ddf0853469445161a9d4d
GiselePark <hanied04@gmail.com>
2024-02-26 18:33:08 +0900

    Feat - Main

--- Main.java
+++ Main.java
@@ -1,8 +1,25 @@
+import view.BoardView;
+import view.GradeView;
+import view.JoinView;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("0-종료, 1-회원가입 2-성적표 3-게시판 4-카우프");
+            switch (sc.next()){
+                case "0": return;
+                //main(String[] args)를 main()으로 수정해서 가져옴
+                case "1": JoinView.main(sc);break;
+                case "2": GradeView.main(sc);break;
+                case "3": BoardView.main();break;
+                case "4": break;
+            }
+        }
     }
 }
